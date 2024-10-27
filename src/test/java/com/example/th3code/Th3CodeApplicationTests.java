e565fb380184d4ad4d4314057bac8c5fea277c7d
Quang <quanglm042000@gmail.com>
2024-10-27 18:00:06 +0700

    Update

---
+++ src/test/java/com/example/th3code/Th3CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.th3code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Th3CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
